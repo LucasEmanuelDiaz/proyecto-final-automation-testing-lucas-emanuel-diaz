f20eb1187b8cda816aa274961ccbba795d708f07
Lucas Emanuel Diaz <Lucas_E_Diaz@hotmail.com>
2025-12-10 16:43:52 -0300

    Reestructurando el proyecto

--- pages/cart_page.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-from pages.base_page import BasePage
-
-class CartPage(BasePage):
-    CHECKOUT_BTN = (By.ID, 'checkout')
-
-    def checkout(self):
-        self.click(self.CHECKOUT_BTN)
