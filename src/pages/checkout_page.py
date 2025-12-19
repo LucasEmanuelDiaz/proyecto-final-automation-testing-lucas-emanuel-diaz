5b20d6f4d1c8663d6fe2071c3275bdaee4d25efd
Lucas Emanuel Diaz <Lucas_E_Diaz@hotmail.com>
2025-12-19 03:59:14 -0300

    Solucionando problemas de reconocimiento de finish()

--- src/pages/checkout_page.py
+++ src/pages/checkout_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 class CheckoutPage:
 
@@ -15,13 +16,33 @@ class CheckoutPage:
         self.wait = WebDriverWait(driver, 15)
 
     def fill_info_and_continue(self, first, last, zip_code):
+    # Confirmar step one
         self.wait.until(EC.url_contains("checkout-step-one"))
 
-        self.wait.until(EC.visibility_of_element_located(self.FIRST_NAME)).send_keys(first)
-        self.wait.until(EC.visibility_of_element_located(self.LAST_NAME)).send_keys(last)
-        self.wait.until(EC.visibility_of_element_located(self.POSTAL_CODE)).send_keys(zip_code)
+        first_name = self.wait.until(EC.element_to_be_clickable(self.FIRST_NAME))
+        last_name = self.wait.until(EC.element_to_be_clickable(self.LAST_NAME))
+        postal = self.wait.until(EC.element_to_be_clickable(self.POSTAL_CODE))
 
-        self.wait.until(EC.element_to_be_clickable(self.CONTINUE)).click()
+        first_name.clear()
+        first_name.send_keys(first)
+
+        last_name.clear()
+        last_name.send_keys(last)
+
+        postal.clear()
+        postal.send_keys(zip_code)
+
+        # 🔥 SUBMIT REAL DEL FORMULARIO
+        postal.send_keys(Keys.ENTER)
+
+        # ✅ ESPERAR que la página siguiente esté lista
+        self.wait.until(EC.presence_of_element_located(self.FINISH))
+
+
 
     def finish(self):
-        self.wait.until(EC.element_to_be_clickable(self.FINISH)).click()
+        finish_btn = self.wait.until(
+        EC.element_to_be_clickable(self.FINISH)
+    )
+        finish_btn.click()
+
